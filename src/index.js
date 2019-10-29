97411aab104a1b78d076ce91aec2472fc7ac92bd
Lucas Vieira <tiomedianope@gmail.com>
2019-10-29 17:21:24 -0300

    Desafio Aula 021

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 const $root = document.querySelector('#root')
 const $memoryCard = document.createElement('article')
+const $memoryCardRound = document.createElement('article')
 const $icon = `
     <img 
         src='img/icon-collabcode.png'
         alt='Gueio mascote da CollaCode'
         class='icon'
      />
-;`
+`;
+
+const $iconJS = `
+    <img
+        src='img/icon-js.png'
+        alt='Livro Javascript'
+        class='icon'
+    />
+`;
 
 $memoryCard.classList.add('memory-card')
+$memoryCardRound.classList.add('memory-card', '-round')
 
 $root.insertBefore($memoryCard, null)
-$memoryCard.insertAdjacentHTML("afterbegin",$icon)
+$memoryCard.insertAdjacentHTML("afterbegin", $icon)
+
+
+$root.insertBefore($memoryCardRound, null)
+$memoryCardRound.insertAdjacentHTML("afterbegin", $iconJS)
