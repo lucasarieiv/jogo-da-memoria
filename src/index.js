2f15be22020c29a4164ec5f2b6302c21b326239f
Lucas Vieira <tiomedianope@gmail.com>
2019-10-29 20:12:36 -0300

    Desafio Básico - Aula 022

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 const $root = document.querySelector('#root')
 const $memoryCard = document.createElement('article')
-const $memoryCardRound = document.createElement('article')
-const $icon = `
+const $memoryCardFront = document.createElement('article')
+const $memoryCard1 = document.createElement('article')
+const $memoryCard2 = document.createElement('article')
+const $memoryCard3 = document.createElement('article')
+const $memoryCard4 = document.createElement('article')
+const $memoryCard5 = document.createElement('article')
+const $memoryCard6 = document.createElement('article')
+const $memoryCard7 = document.createElement('article')
+const $memoryCard8 = document.createElement('article')
+
+const $iconCollab = `
     <img 
         src='img/icon-collabcode.png'
         alt='Gueio mascote da CollaCode'
@@ -17,12 +26,43 @@ const $iconJS = `
     />
 `;
 
-$memoryCard.classList.add('memory-card')
-$memoryCardRound.classList.add('memory-card', '-round')
 
 $root.insertBefore($memoryCard, null)
-$memoryCard.insertAdjacentHTML("afterbegin", $icon)
+$root.insertBefore($memoryCardFront, null)
+$root.insertBefore($memoryCard1, null)
+$root.insertBefore($memoryCard2, null)
+$root.insertBefore($memoryCard3, null)
+$root.insertBefore($memoryCard4, null)
+$root.insertBefore($memoryCard5, null)
+$root.insertBefore($memoryCard6, null)
+$root.insertBefore($memoryCard7, null)
+$root.insertBefore($memoryCard8, null)
 
 
-$root.insertBefore($memoryCardRound, null)
-$memoryCardRound.insertAdjacentHTML("afterbegin", $iconJS)
+$memoryCard.classList.add('memory-card')
+$memoryCard1.classList.add('memory-card')
+$memoryCard2.classList.add('memory-card')
+$memoryCard3.classList.add('memory-card')
+$memoryCard4.classList.add('memory-card')
+$memoryCard5.classList.add('memory-card')
+$memoryCard6.classList.add('memory-card')
+$memoryCard7.classList.add('memory-card')
+$memoryCard8.classList.add('memory-card')
+
+$memoryCardFront.classList.add('memory-card', '-front')
+
+
+
+$memoryCard1.insertAdjacentHTML("afterbegin", $iconCollab)
+$memoryCard2.insertAdjacentHTML("afterbegin", $iconCollab)
+$memoryCard3.insertAdjacentHTML("afterbegin", $iconCollab)
+$memoryCard4.insertAdjacentHTML("afterbegin", $iconCollab)
+$memoryCard5.insertAdjacentHTML("afterbegin", $iconCollab)
+$memoryCard6.insertAdjacentHTML("afterbegin", $iconCollab)
+$memoryCard7.insertAdjacentHTML("afterbegin", $iconCollab)
+$memoryCard8.insertAdjacentHTML("afterbegin", $iconCollab)
+
+
+$memoryCard.insertAdjacentHTML("afterbegin", $iconCollab)
+$memoryCardFront.insertAdjacentHTML("afterbegin", $iconJS)
+
