20f2faa758dcbf0bbb6d7fd4527f2c8ae20ca073
Lucas Vieira <tiomedianope@gmail.com>
2019-11-04 18:18:56 -0300

    Desafio 1 - Aula 024

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,35 +1,23 @@
-function createMemoryCard() {
-
-    const $memoryCard = `
-        <article class="memory-card">
-            <img 
-                src="img/icon-collabcode.png"
-                alt="Logo do mascote da CollabCode"
-                class="icon"
-                onClick="handleClick()"
-            />
-        </article>
-    `
-
-    return $memoryCard
-}
-
-function createMemoryCardFront() {
-
-    const $memoryCardFront = `
-        <article class="memory-card -front">
-            <img 
-                src="img/icon-js.png"
-                alt="Icone livro sobre javascript"
-                class="icon"
-                onClick="handleClick()"
-            />
-        </article>
-    `
-
-    return $memoryCardFront
-}
 
 function handleClick() {
     console.log('Cartão Sharkão, Yam Yam')
 };
+
+const $memoryCard = () => {
+    const memoryCard = ` 
+    <article class="memory-card">
+    <img src="img/icon-collabcode.png" alt="Icone livro sobre javascript" class="icon" onClick="handleClick()" />
+    </article> `;
+    
+    return memoryCard
+}
+
+const $memoryCardFront = () => {
+    const memoryCardFront = `
+    <article class="memory-card -front">
+    <img src="img/icon-js.png" alt="Icone livro sobre javascript" class="icon" onClick="handleClick()" />
+    </article> `;
+
+    return memoryCardFront
+}
+
--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,17 +1,22 @@
 const $root = document.querySelector('#root')
 
 const $cardsWrapper = createCardsWrapper()
-const $memoryCard = createMemoryCard()
-const $memoryCardFront = createMemoryCardFront()
 
-$cardsWrapper.insertAdjacentHTML('afterbegin', $memoryCard)
-$cardsWrapper.insertAdjacentHTML('afterbegin', $memoryCard)
-$cardsWrapper.insertAdjacentHTML('afterbegin', $memoryCard)
-$cardsWrapper.insertAdjacentHTML('afterbegin', $memoryCardFront)
-$cardsWrapper.insertAdjacentHTML('afterbegin', $memoryCardFront)
-$cardsWrapper.insertAdjacentHTML('afterbegin', $memoryCard)
-$cardsWrapper.insertAdjacentHTML('afterbegin', $memoryCard)
-$cardsWrapper.insertAdjacentHTML('afterbegin', $memoryCard)
+const memoryCard = $memoryCard()
+const memoryCardFront = $memoryCardFront()
+
+$cardsWrapper.insertAdjacentHTML('afterbegin', memoryCard)
+$cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardFront)
+$cardsWrapper.insertAdjacentHTML('afterbegin', memoryCard)
+$cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardFront)
+$cardsWrapper.insertAdjacentHTML('afterbegin', memoryCard)
+$cardsWrapper.insertAdjacentHTML('afterbegin', memoryCard)
+$cardsWrapper.insertAdjacentHTML('afterbegin', memoryCard)
+$cardsWrapper.insertAdjacentHTML('afterbegin', memoryCard)
+
+// $cardsWrapper.insertAdjacentHTML('afterbegin', $memoryCard)
+// $cardsWrapper.insertAdjacentHTML('afterbegin', $memoryCard)
+
 
 $root.insertAdjacentElement('beforeend', $cardsWrapper)
 
