8a03bf0c7a5a36d8d7229090edab7ac392dac711
lucasarieiv <w3desenvolvedor@gmail.com>
2019-11-28 11:45:21 -0300

    Final Aula 031

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,47 +1,49 @@
-const $root = document.querySelector('#root')
+(function () {
+    const $root = document.querySelector('#root')
+    
+    const $cardsWrapper = createCardsWrapper()
+    
+    const createMemoryCard = memoryCard()
+    
+    const memoryCardC = createMemoryCard(
+        {
+            nameClass: "-front",
+            src: "img/icon-c.png",
+            alt: "Icone do Livro de C"
+        }
+    );
+    
+    const memoryCardBug = createMemoryCard(
+        {
+            src: "img/icon-bug.png",
+            alt: "Icone de um Inseto"
+        }
+    );
+    
+    const memoryCardJS = createMemoryCard(
+        {
+            src: "img/icon-js.png",
+            alt: "Icone do Livro de Javascript"
+        }
+    );
+    
+    const memoryCardJava = createMemoryCard(
+        {
+            src: "img/icon-php.png",
+            alt: "Icone Livro de Php"
+        }
+    )
+    
+    
+    $cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardBug)
+    $cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardC)
+    $cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardJava)
+    $cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardBug)
+    $cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardJS)
+    $cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardC)
+    $cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardJava)
+    $cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardJS)
+    
+    $root.insertAdjacentElement('beforeend', $cardsWrapper)
 
-const $cardsWrapper = createCardsWrapper()
-
-const createMemoryCard = memoryCard()
-
-const memoryCardC = createMemoryCard(
-    {
-        nameClass: "-front",
-        src: "img/icon-c.png",
-        alt: "Icone do Livro de C"
-    }
-);
-
-const memoryCardBug = createMemoryCard(
-    {
-        src: "img/icon-bug.png",
-        alt: "Icone de um Inseto"
-    }
-);
-
-const memoryCardJS = createMemoryCard(
-    {
-        src: "img/icon-js.png",
-        alt: "Icone do Livro de Javascript"
-    }
-);
-
-const memoryCardJava = createMemoryCard(
-    {
-        src: "img/icon-php.png",
-        alt: "Icone Livro de Php"
-    }
-)
-
-
-$cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardBug)
-$cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardC)
-$cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardJava)
-$cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardBug)
-$cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardJS)
-$cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardC)
-$cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardJava)
-$cardsWrapper.insertAdjacentHTML('afterbegin', memoryCardJS)
-
-$root.insertAdjacentElement('beforeend', $cardsWrapper)
-
+}) ();    
