9bf40995aca9ffde2fd0c5b47e7b0e6591ee7a3c
lucasarieiv <w3desenvolvedor@gmail.com>
2020-01-04 00:08:13 -0300

    Desafio Intermediario - Aula 033

--- src/components/start-btn/index.js
+++ src/components/start-btn/index.js
@@ -1,12 +1,40 @@
 const startBtn = (function () {
     const module = {};
 
-    module.create = function() {
-        return `
-            <button>Start</button>
+    module._style = function() {
+        const $head = document.querySelector('head')
+        const $style = document.createElement("style");
+        
+        $style.textContent = `
+            .btn-start {
+                display: block;
+                width: 100px;
+                height: 100px;
+                font-size: 1.2em;
+                border-radius: 100px;
+                border: none;
+                background-color: #fff;
+                border: 4px solid #3a4042;
+                color: #3a4042;
+                position: absolute;
+                bottom: 10%;
+                left: 50%;
+                transform: translateX(-50%);
+            }
+
         `
+
+        $head.insertBefore($style, null)
     }
     
+    module.create = function() {
+        module._style();
+
+        return `
+            <button class="btn-start">Start</button>
+        `
+    }
+
     return {
         create: module.create
     }
