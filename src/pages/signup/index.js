9e95fdd5ee7397c1dae6036a9bedbf80a5791100
lucasarieiv <w3desenvolvedor@gmail.com>
2020-01-15 21:13:05 -0300

    Desafio Intermediario - Aula 038

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,8 +1,8 @@
 (function() {
     const $root = document.querySelector('#root')
     const $flatButton = flatButton.render("Log In")
-    const $flatButtonActive = flatButton.render("Log In")
+    const $flatButtonActive = flatButton.render("Sign Up")
 
     $root.insertAdjacentHTML("afterbegin", $flatButton)
-    $root.insertAdjacentHTML("afterbegin", $flatButtonActive)
+    $root.insertAdjacentHTML("afterend", $flatButtonActive)
 })()
