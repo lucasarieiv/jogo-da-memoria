35fa3960590cb1dbf85cca6397467d1511b303e7
lucasarieiv <w3desenvolvedor@gmail.com>
2020-03-04 14:32:41 -0300

    Adicionou a imagem eye.png por Css

--- src/objects/eye-collabcode/index.js
+++ src/objects/eye-collabcode/index.js
@@ -1,13 +1,31 @@
 const eyeCollabcode = (function() {
     const module = {};
 
-    module._style = () => {};
+    module._style = () => {
+        const $head = document.querySelector("head");
+        const $style = document.createElement("style");
+
+        $style.textContent = `
+            .eye-collabcode  {
+                background-image: url(/img/eye.png);
+                background-repeat: no-repeat;
+                background-position: center;
+                display: block;
+                text-indent: -9999px;
+                width: 24px;
+                height: 15px;
+            }
+            
+        `;
+
+        $head.insertAdjacentElement("beforeend", $style)
+    };
 
     module.render = () => {
         module._style();
         
         return `
-            <label>Mostrar senha</label>
+            <label class="eye-collabcode">Mostrar senha</label>
         `;
     }
     
