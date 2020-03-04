c37cd05507fd5581492f8023503143353624d4a7
lucasarieiv <w3desenvolvedor@gmail.com>
2020-03-04 10:33:45 -0300

    Desafio Basico - Aula 044

--- src/objects/input-collabcode/index.js
+++ src/objects/input-collabcode/index.js
@@ -1,11 +1,21 @@
 const inputCollabcode = (function() {
     const module = {}
 
+    module.handleClick = (event) => {
+        const $img = event.target;
+        $img.classList.toggle("-active");
+    }
+
     module._style = () => {
         const $head = document.querySelector("head");
         const $style = document.createElement("style");
         
         $style.textContent = `
+
+            .input-container {
+                position: relative;
+            }
+
             .input-collabcode {
                 display: block;
                 width: 100%;
@@ -15,9 +25,22 @@ const inputCollabcode = (function() {
                 border-bottom: 2px solid rgba(58, 64, 66, .5);
                 padding: 12px 0;
             }
-            .input-collabcode + .label-collabcode {
+            .input-container + .label-collabcode {
                 margin-top: 30px;
             }
+
+            .input-hidden {
+                position: absolute;
+                top: 50%;
+                transform: translateY(-50%);
+                right: 0;
+                opacity: .5;
+            }
+
+            .input-hidden.-active {
+                opacity: 1;
+            }
+
         `
 
         $head.insertAdjacentElement("beforeend", $style)
@@ -27,11 +50,15 @@ const inputCollabcode = (function() {
         module._style();
 
         return `
-            <input class="input-collabcode" type="${type}" placeholder="${placeholder}"/>
+            <div class="input-container">
+                <input class="input-collabcode" type="${type}" placeholder="${placeholder}"/>
+                ${type == 'password' ? '<img onClick="inputCollabcode.handleClick(event)" class="input-hidden" src="../../../img/hidden.png" alt="Icone olho">' : ''}
+            </div>
         `
     }
 
     return {
-        render: module.render
+        render: module.render,
+        handleClick: module.handleClick
     } 
 })();
