0db0baead83cd093c647972525437ebccd2417ec
lucasarieiv <w3desenvolvedor@gmail.com>
2020-03-06 13:29:39 -0300

    Desafio Intermediario - Aula 045

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@
     const {hash} = window.location;
 
     const paths = [];
+    console.log(paths)
+    paths[`${hash}`] = error;
     paths[""] = login;
     paths["#/game"] = game;
     paths["#/login"] = login;
     paths["#/signup"] = signup;
+    console.log("Minha hash é " + hash)
 
     paths[hash]();
 
---
+++ src/pages/error404/index.js
@@ -0,0 +1,14 @@
+const error = (function() {
+    const $root = document.querySelector("#root");
+
+    const $loginButton = flatButton.render("Log In", true, "login");
+    const $signUpButton = flatButton.render("Sign Up", false, "signup");
+
+    const $logoCollabcode = logoCollabcode.render();
+    const $titleCollabcode = titleCollabcode.render("Error 404 - Página não encontrada");
+
+    const $logoWrapper = logoWrapper.render($logoCollabcode, $titleCollabcode);
+    $root.insertAdjacentHTML("beforeend", $loginButton);
+    $root.insertAdjacentHTML("beforeend", $signUpButton);
+    $root.insertAdjacentHTML("beforeend", $logoWrapper);
+});
